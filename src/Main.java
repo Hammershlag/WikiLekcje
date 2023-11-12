1d09e5939e915883cb0beafa5c303c5d81cab045
tomek <tomekzbroszczyk@gmail.com>
2023-11-12 12:42:57 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+     * @author  ${AUTHOR} on 12.11.2023
+     * @version ${VERSION}
+     */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
